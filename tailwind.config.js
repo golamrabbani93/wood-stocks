8ee70f51ab8cd1202d589a8405dbe2dc81160e83
golamrabbani93 <golamrabbani9373@gmail.com>
2022-11-23 21:38:26 +0600

    set theme color

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
 	content: ['./src/**/*.{js,jsx,ts,tsx}'],
+	daisyui: {
+		themes: [
+			{
+				doctortheme: {
+					primary: '#6C4AB6',
+					secondary: '#8D72E1',
+					accent: '#3A4256',
+					neutral: '#3D4451',
+					'base-100': '#FFFFFF',
+				},
+			},
+		],
+	},
 	theme: {
 		extend: {},
 	},
