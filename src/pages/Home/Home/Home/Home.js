be57ce8808a69a46104e0d9e2815b653eb664157
golamrabbani93 <golamrabbani9373@gmail.com>
2022-11-23 22:47:03 +0600

    create title hook And title dynamic

--- src/pages/Home/Home/Home/Home.js
+++ src/pages/Home/Home/Home/Home.js
@@ -1,7 +1,9 @@
 import React from 'react';
+import UseTitle from '../../../../hooks/UseTitle';
 import Banner from '../Banner/Banner';
 
 const Home = () => {
+	UseTitle('Home | Sitpad');
 	return (
 		<div>
 			<Banner></Banner>
