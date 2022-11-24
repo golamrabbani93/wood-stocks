10fbe1e76b36c61cd3da909b7a082b5ad2098680
golamrabbani93 <golamrabbani9373@gmail.com>
2022-11-24 21:24:52 +0600

    check current user

--- src/Context/AuthProvider.js
+++ src/Context/AuthProvider.js
@@ -1,12 +1,18 @@
-import React, {createContext, useState} from 'react';
-import {createUserWithEmailAndPassword, getAuth, signInWithEmailAndPassword} from 'firebase/auth';
+import React, {createContext, useEffect, useState} from 'react';
+import {
+	createUserWithEmailAndPassword,
+	getAuth,
+	onAuthStateChanged,
+	signInWithEmailAndPassword,
+} from 'firebase/auth';
 import app from '../firebase/firebase.config';
 
 export const AuthContext = createContext();
 const AuthProvider = ({children}) => {
 	const auth = getAuth(app);
 
-	const [user, setUser] = useState('Rabbani');
+	const [user, setUser] = useState([]);
+	console.log('🚀🚀: AuthProvider -> user', user);
 
 	//*create user
 	const createUser = (email, password) => {
@@ -16,6 +22,15 @@ const AuthProvider = ({children}) => {
 	const LoginUser = (email, password) => {
 		return signInWithEmailAndPassword(auth, email, password);
 	};
+	//* cuurent user check
+	useEffect(() => {
+		const unSubscribed = onAuthStateChanged(auth, (curentUser) => {
+			setUser(curentUser);
+		});
+		return () => {
+			unSubscribed();
+		};
+	}, [auth]);
 	const info = {user, createUser, LoginUser};
 	return <AuthContext.Provider value={info}>{children}</AuthContext.Provider>;
 };
