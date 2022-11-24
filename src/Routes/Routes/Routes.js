daf0337e8f86bd17f4f17327ee4311c620413dac
golamrabbani93 <golamrabbani9373@gmail.com>
2022-11-24 13:17:44 +0600

    make dynamic url

--- src/Routes/Routes/Routes.js
+++ src/Routes/Routes/Routes.js
@@ -1,6 +1,7 @@
 import {createBrowserRouter} from 'react-router-dom';
 import Main from '../../Layouts/Main/Main';
 import Home from '../../pages/Home/Home/Home/Home';
+import CategoryProducts from '../../pages/Shared/CategoryProducts/CategoryProducts';
 
 export const router = createBrowserRouter([
 	{
@@ -10,6 +11,11 @@ export const router = createBrowserRouter([
 			{
 				path: '/',
 				element: <Home></Home>,
+			},
+			{
+				path: '/products/:category_id',
+				loader: (params) => fetch(`http://localhost:5000/products/${params.category_id}`),
+				element: <CategoryProducts></CategoryProducts>,
 			},
 		],
 	},
---
+++ src/pages/Shared/CategoryProducts/CategoryProducts.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const CategoryProducts = () => {
+	return (
+		<div>
+			<h2>category products</h2>
+		</div>
+	);
+};
+
+export default CategoryProducts;
