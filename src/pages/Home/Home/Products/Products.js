ef48688ac7111a8c2252762e9f846fef35b43995
golamrabbani93 <golamrabbani9373@gmail.com>
2022-11-24 12:40:58 +0600

    setup query client

--- src/pages/Home/Home/Products/Products.js
+++ src/pages/Home/Home/Products/Products.js
@@ -1,10 +1,20 @@
+import {useQuery} from '@tanstack/react-query';
 import React from 'react';
 
 const Products = () => {
+	const {data: productsCategory = []} = useQuery({
+		queryKey: ['appointmentOptions'],
+		queryFn: async () => {
+			const res = await fetch('http://localhost:5000/categories');
+			const data = await res.json();
+			return data;
+		},
+	});
+
 	return (
 		<div className="">
 			<h2 className="text-primary text-4xl font-semibold">Product Categories</h2>
-			<div className="mt-6">g</div>
+			<div className="mt-6">{productsCategory.length}</div>
 		</div>
 	);
 };
