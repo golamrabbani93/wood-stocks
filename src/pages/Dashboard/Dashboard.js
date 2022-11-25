e5099162b6c3fc01b7f58520213c834bd4c300b5
golamrabbani93 <golamrabbani9373@gmail.com>
2022-11-25 15:11:39 +0600

    add nested drawer on dashboard

--- src/pages/Dashboard/Dashboard.js
+++ src/pages/Dashboard/Dashboard.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import UseTitle from '../../hooks/UseTitle';
 
 const Dashboard = () => {
+	UseTitle('Dashboard | Woods Stoks');
 	return (
-		<div>
+		<div className="text-left">
 			<h2>this is Dashboard</h2>
 		</div>
 	);
