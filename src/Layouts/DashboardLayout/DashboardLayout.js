d2b79970b7e5dc57fe157111d4a0cedcfefcce74
golamrabbani93 <golamrabbani9373@gmail.com>
2022-11-28 00:43:53 +0600

    nav setup

--- src/Layouts/DashboardLayout/DashboardLayout.js
+++ src/Layouts/DashboardLayout/DashboardLayout.js
@@ -14,9 +14,6 @@ const DashboardLayout = () => {
 				<input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
 				<div className="drawer-content">
 					<Outlet></Outlet>
-					<label htmlFor="my-drawer-2" className="btn btn-primary drawer-button lg:hidden">
-						Open drawer
-					</label>
 				</div>
 				<div className="drawer-side  mr-3">
 					<label htmlFor="my-drawer-2" className="drawer-overlay"></label>
--- src/pages/Shared/Navbar/Navbar.js
+++ src/pages/Shared/Navbar/Navbar.js
@@ -19,52 +19,51 @@ const Navbar = () => {
 	return (
 		<header className="relative z-10 ">
 			<div className="navbar container mx-auto">
-				<div className="navbar-start">
-					<div className="dropdown">
-						<label className="btn btn-circle swap swap-rotate lg:hidden bg-transparent hover:bg-transparent text-accent border-0 font-bold">
-							<input onClick={() => sentMenu(!menu)} type="checkbox" />
-
-							<svg
-								className="swap-off fill-current"
-								xmlns="http://www.w3.org/2000/svg"
-								width="32"
-								height="32"
-								viewBox="0 0 512 512"
+				<div className="navbar-start w-0 lg:w-1/2 mr-auto">
+					<div className="flex justify-between ">
+						<div className="dropdown">
+							<label tabIndex={0} className="btn text-white btn-primary lg:hidden">
+								<svg
+									xmlns="http://www.w3.org/2000/svg"
+									className="h-5 w-5"
+									fill="none"
+									viewBox="0 0 24 24"
+									stroke="currentColor"
+								>
+									<path
+										strokeLinecap="round"
+										strokeLinejoin="round"
+										strokeWidth="2"
+										d="M4 6h16M4 12h8m-8 6h16"
+									/>
+								</svg>
+							</label>
+							<ul
+								tabIndex={0}
+								className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-300 rounded-box w-52 "
 							>
-								<path d="M64,384H448V341.33H64Zm0-106.67H448V234.67H64ZM64,128v42.67H448V128Z" />
-							</svg>
-
-							<svg
-								className="swap-on fill-current"
-								xmlns="http://www.w3.org/2000/svg"
-								width="32"
-								height="32"
-								viewBox="0 0 512 512"
-							>
-								<polygon points="400 145.49 366.51 112 256 222.51 145.49 112 112 145.49 222.51 256 112 366.51 145.49 400 256 289.49 366.51 400 400 366.51 289.49 256 400 145.49" />
-							</svg>
-						</label>
-						{menu ? (
-							<ul className="menu menu-compact  mt-3 p-2 shadow bg-base-100 rounded-box w-52">
 								{navItems}
+								{user?.uid ? (
+									''
+								) : (
+									<div className="navbar-end lg:hidden w-full">
+										<Link to="/login" className="btn">
+											Login
+										</Link>
+									</div>
+								)}
 							</ul>
-						) : (
-							''
-						)}
+						</div>
 					</div>
-					<Link
-						to={'/'}
-						className="btn text-center hidden md:block btn-ghost hover:bg-transparent text-primary font-extrabold normal-case text-3xl"
-					>
-						Woods Stocks
-					</Link>
+					<div className="mr-auto">
+						<Link
+							to="/"
+							className="btn text-center hidden md:block btn-ghost hover:bg-transparent text-primary font-extrabold normal-case text-3xl"
+						>
+							Woods Stock
+						</Link>
+					</div>
 				</div>
-				<Link
-					to={'/'}
-					className="btn text-center normal-case hover:bg-transparent text-primary font-extrabold md:hidden text-xl"
-				>
-					Woods Stock
-				</Link>
 				<div className="navbar-center hidden lg:flex">
 					<ul className="menu menu-horizontal p-0">{navItems}</ul>
 				</div>
@@ -112,6 +111,21 @@ const Navbar = () => {
 						</Link>
 					)}
 				</div>
+				{user?.uid ? (
+					<label htmlFor="my-drawer-2" className="ml-3 btn btn-primary drawer-button lg:hidden">
+						<svg
+							className="swap-off fill-current"
+							xmlns="http://www.w3.org/2000/svg"
+							width="32"
+							height="32"
+							viewBox="0 0 512 512"
+						>
+							<path d="M64,384H448V341.33H64Zm0-106.67H448V234.67H64ZM64,128v42.67H448V128Z" />
+						</svg>
+					</label>
+				) : (
+					''
+				)}
 			</div>
 		</header>
 	);
