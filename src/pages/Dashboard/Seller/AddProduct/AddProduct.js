0eff4c1d4f5339230711c22004c74fcfe62c7145
golamrabbani93 <golamrabbani9373@gmail.com>
2022-11-27 21:26:06 +0600

    add puchase time

--- src/pages/Dashboard/Seller/AddProduct/AddProduct.js
+++ src/pages/Dashboard/Seller/AddProduct/AddProduct.js
@@ -20,6 +20,7 @@ const AddProduct = () => {
 			price: data.price,
 			og_price: data.og_price,
 			uses: data.uses,
+			purchase: data.purchase,
 			seller_name: user.displayName,
 			category_id: parseInt(data.category),
 			productStatus: 'available',
@@ -56,138 +57,149 @@ const AddProduct = () => {
 				<h2 className="text-left text-primary uppercase text-3xl font-bold">Add Product</h2>
 				<form onSubmit={handleSubmit(handleProduct)}>
 					<div className="grid lg:grid-cols-3 gap-5 md:w-[900px]">
-						<div className="left-side">
-							<div className="form-control  max-w-xs">
-								<label className="label">
-									<span className="label-text">Product Name</span>
-								</label>
-								<input
-									type="text"
-									{...register('name', {
-										required: 'Please Enter Product Name',
-									})}
-									className="input input-bordered input-primary  max-w-xs"
-								/>
-								<span className="mt-1">
-									{errors.name && <p className="text-red-500">{errors.name.message}</p>}
-								</span>
-							</div>
-							<div className="form-control  max-w-xs">
-								<label className="label">
-									<span className="label-text">Price</span>
-								</label>
-								<input
-									type="text"
-									{...register('price', {
-										required: 'Please Enter price',
-									})}
-									className="input input-bordered input-primary  max-w-xs"
-								/>
-								<span className="mt-1">
-									{errors.email && <p className="text-red-500">{errors.email.message}</p>}
-								</span>
-							</div>
-
-							<div className="form-control  max-w-xs">
-								<label className="label">
-									<span className="label-text">Condition</span>
-								</label>
-								<select {...register('condition')} className="select select-primary  max-w-xs">
-									<option>Excellent</option>
-									<option value="Good">Good</option>
-									<option value="bad">bad</option>
-								</select>
-							</div>
+						<div className="form-control  max-w-xs">
+							<label className="label">
+								<span className="label-text">Product Name</span>
+							</label>
+							<input
+								type="text"
+								{...register('name', {
+									required: 'Please Enter Product Name',
+								})}
+								className="input input-bordered input-primary  max-w-xs"
+							/>
+							<span className="mt-1">
+								{errors.name && <p className="text-red-500">{errors.name.message}</p>}
+							</span>
 						</div>
-						<div className="middle-side">
-							<div className="form-control  max-w-xs">
-								<label className="label">
-									<span className="label-text">Phone Number</span>
-								</label>
-								<input
-									type="text"
-									{...register('phone', {
-										required: 'Please Enter Your Number',
-									})}
-									className="input input-bordered input-primary  max-w-xs"
-								/>
-								<span className="mt-1">
-									{errors.name && <p className="text-red-500">{errors.name.message}</p>}
-								</span>
-							</div>
-							<div className="form-control  max-w-xs">
-								<label className="label">
-									<span className="label-text">Location</span>
-								</label>
-								<select {...register('location')} className="select select-primary  max-w-xs">
-									<option>Dhaka</option>
-									<option value="Rajshahi">Rajshahi</option>
-									<option value="Khulna">Khulna</option>
-									<option value="Chittagong">Chittagong</option>
-									<option value="Cumilla">Cumilla</option>
-								</select>
-								<span className="mt-1">
-									{errors.email && <p className="text-red-500">{errors.email.message}</p>}
-								</span>
-							</div>
-
-							<div className="form-control  max-w-xs">
-								<label className="label">
-									<span className="label-text">Product Category</span>
-								</label>
-								<select {...register('category')} className="select select-primary  max-w-xs">
-									<option value={1}>Home</option>
-									<option value="2">Kichen</option>
-									<option value="3">Office</option>
-								</select>
-							</div>
+						<div className="form-control  max-w-xs">
+							<label className="label">
+								<span className="label-text">Resale Price</span>
+							</label>
+							<input
+								type="text"
+								{...register('price', {
+									required: 'Please Enter Resale price',
+								})}
+								className="input input-bordered input-primary  max-w-xs"
+							/>
+							<span className="mt-1">
+								{errors.email && <p className="text-red-500">{errors.email.message}</p>}
+							</span>
 						</div>
-						<div className="right-side">
-							<div className="form-control  max-w-xs">
-								<label className="label">
-									<span className="label-text">Year of purchase</span>
-								</label>
-								<input
-									type="text"
-									{...register('uses', {
-										required: 'Please Enter Year of purchase',
-									})}
-									className="input input-bordered input-primary  max-w-xs"
-								/>
-								<span className="mt-1">
-									{errors.name && <p className="text-red-500">{errors.name.message}</p>}
-								</span>
-							</div>
-							<div className="form-control  max-w-xs">
-								<label className="label">
-									<span className="label-text">Product Image</span>
-								</label>
-								<input
-									type="text"
-									{...register('img', {
-										required: 'Please Enter Image Url',
-									})}
-									className="input input-bordered input-primary  max-w-xs"
-								/>
-								<span className="mt-1">
-									{errors.name && <p className="text-red-500">{errors.name.message}</p>}
-								</span>
-							</div>
-							<div className="form-control  max-w-xs">
-								<label className="label">
-									<span className="label-text">Orginal Price</span>
-								</label>
-								<input
-									type="text"
-									{...register('og_price', {
-										required: 'Orginal Price',
-									})}
-									className="input input-bordered input-primary  max-w-xs"
-								/>
-								<span className="mt-1">
-									{errors.name && <p className="text-red-500">{errors.name.message}</p>}
-								</span>
-							</div>
+
+						<div className="form-control  max-w-xs">
+							<label className="label">
+								<span className="label-text">Condition</span>
+							</label>
+							<select {...register('condition')} className="select select-primary  max-w-xs">
+								<option>Excellent</option>
+								<option value="Good">Good</option>
+								<option value="fair">fair</option>
+							</select>
+						</div>
+
+						<div className="form-control  max-w-xs">
+							<label className="label">
+								<span className="label-text">Phone Number</span>
+							</label>
+							<input
+								type="text"
+								{...register('phone', {
+									required: 'Please Enter Your Number',
+								})}
+								className="input input-bordered input-primary  max-w-xs"
+							/>
+							<span className="mt-1">
+								{errors.name && <p className="text-red-500">{errors.name.message}</p>}
+							</span>
+						</div>
+						<div className="form-control  max-w-xs">
+							<label className="label">
+								<span className="label-text">Location</span>
+							</label>
+							<select {...register('location')} className="select select-primary  max-w-xs">
+								<option>Dhaka</option>
+								<option value="Rajshahi">Rajshahi</option>
+								<option value="Khulna">Khulna</option>
+								<option value="Chittagong">Chittagong</option>
+								<option value="Cumilla">Cumilla</option>
+							</select>
+							<span className="mt-1">
+								{errors.email && <p className="text-red-500">{errors.email.message}</p>}
+							</span>
+						</div>
+
+						<div className="form-control  max-w-xs">
+							<label className="label">
+								<span className="label-text">Product Category</span>
+							</label>
+							<select {...register('category')} className="select select-primary  max-w-xs">
+								<option value={1}>Home</option>
+								<option value="2">Kichen</option>
+								<option value="3">Office</option>
+							</select>
+						</div>
+
+						<div className="form-control  max-w-xs">
+							<label className="label">
+								<span className="label-text">Year of Uses</span>
+							</label>
+							<input
+								type="text"
+								{...register('uses', {
+									required: 'Please Enter Year of Uses',
+								})}
+								className="input input-bordered input-primary  max-w-xs"
+							/>
+							<span className="mt-1">
+								{errors.name && <p className="text-red-500">{errors.name.message}</p>}
+							</span>
+						</div>
+						<div className="form-control  max-w-xs">
+							<label className="label">
+								<span className="label-text">Product Image Url</span>
+							</label>
+							<input
+								type="text"
+								{...register('img', {
+									required: 'Please Enter Image Url',
+								})}
+								className="input input-bordered input-primary  max-w-xs"
+							/>
+							<span className="mt-1">
+								{errors.name && <p className="text-red-500">{errors.name.message}</p>}
+							</span>
+						</div>
+						<div className="form-control  max-w-xs">
+							<label className="label">
+								<span className="label-text">Orginal Price</span>
+							</label>
+							<input
+								type="text"
+								{...register('og_price', {
+									required: 'Orginal Price',
+								})}
+								className="input input-bordered input-primary  max-w-xs"
+							/>
+							<span className="mt-1">
+								{errors.name && <p className="text-red-500">{errors.name.message}</p>}
+							</span>
+						</div>
+						<div className="form-control  max-w-xs">
+							<label className="label">
+								<span className="label-text">Year of purchase</span>
+							</label>
+							<input
+								type="text"
+								{...register('purchase', {
+									required: 'Please Enter Year of purchase',
+								})}
+								className="input input-bordered input-primary  max-w-xs"
+							/>
+							<span className="mt-1">
+								{errors.name && <p className="text-red-500">{errors.name.message}</p>}
+							</span>
 						</div>
 					</div>
 
