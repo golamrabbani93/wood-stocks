eed6253881ddefa4faef914a35ed339d2de7888d
golamrabbani93 <golamrabbani9373@gmail.com>
2022-11-28 22:10:00 +0600

    create extra section

--- src/pages/Home/Home/Home/Home.js
+++ src/pages/Home/Home/Home/Home.js
@@ -2,6 +2,7 @@ import React from 'react';
 import UseTitle from '../../../../hooks/UseTitle';
 import AdvertiseProduct from '../AdvertiseProduct/AdvertiseProduct';
 import Banner from '../Banner/Banner';
+import MakeUs from '../MakeUS/MakeUs';
 import Products from '../Products/Products';
 
 const Home = () => {
@@ -11,6 +12,7 @@ const Home = () => {
 			<Banner></Banner>
 			<Products></Products>
 			<AdvertiseProduct></AdvertiseProduct>
+			<MakeUs></MakeUs>
 		</div>
 	);
 };
